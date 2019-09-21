5c4875cb4fe72b3d7fa4c2b64d6727f8c38fcbfc
AdhamMoussa <adam.moussa62017@gmail.com>
2019-09-21 13:39:44 +0200

    filters screen

--- src/screens/Filters.tsx
+++ src/screens/Filters.tsx
@@ -1,14 +1,90 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { View, Text, Switch, StyleSheet } from "react-native";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import CustomHeaderButton from "../components/CustomHeaderButton";
 
+import { defaultTheme } from "../utils/theme";
+
 const Filters: NavigationStackScreenComponent = props => {
+  const { navigation } = props;
+
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const saveFilters = useCallback(() => {
+    const currentFilters = {
+      isGlutenFree,
+      isLactoseFree,
+      isVegan,
+      isVegetarian
+    };
+
+    console.log(currentFilters);
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+  useEffect(() => {
+    navigation.setParams({ saveFilters });
+  }, [saveFilters]);
+
   return (
     <View>
-      <Text>Filters screen</Text>
+      <Text style={styles.title}>Filters</Text>
+
+      <View style={styles.filter}>
+        <Text style={styles.filterLabel}>Gluten-free</Text>
+        <Switch
+          value={isGlutenFree}
+          onValueChange={setIsGlutenFree}
+          trackColor={{
+            true: defaultTheme.colors.main,
+            false: defaultTheme.colors.gray
+          }}
+          thumbColor={defaultTheme.colors.gray}
+        />
+      </View>
+
+      <View style={styles.filter}>
+        <Text style={styles.filterLabel}>Lactose-free</Text>
+        <Switch
+          value={isLactoseFree}
+          onValueChange={setIsLactoseFree}
+          trackColor={{
+            true: defaultTheme.colors.main,
+            false: defaultTheme.colors.gray
+          }}
+          thumbColor={defaultTheme.colors.gray}
+        />
+      </View>
+
+      <View style={styles.filter}>
+        <Text style={styles.filterLabel}>Vegan</Text>
+        <Switch
+          value={isVegan}
+          onValueChange={setIsVegan}
+          trackColor={{
+            true: defaultTheme.colors.main,
+            false: defaultTheme.colors.gray
+          }}
+          thumbColor={defaultTheme.colors.gray}
+        />
+      </View>
+
+      <View style={styles.filter}>
+        <Text style={styles.filterLabel}>Vegetarian</Text>
+        <Switch
+          value={isVegetarian}
+          onValueChange={setIsVegetarian}
+          trackColor={{
+            true: defaultTheme.colors.main,
+            false: defaultTheme.colors.gray
+          }}
+          thumbColor={defaultTheme.colors.gray}
+        />
+      </View>
     </View>
   );
 };
@@ -19,13 +95,41 @@ Filters.navigationOptions = navProps => ({
     <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
       <Item
         iconName="md-menu"
-        title="drawer icon"
+        title="menu"
         onPress={() => {
           navProps.navigation.toggleDrawer();
         }}
       />
     </HeaderButtons>
+  ),
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item
+        iconName="ios-save"
+        title="save"
+        onPress={navProps.navigation.getParam("saveFilters")}
+      />
+    </HeaderButtons>
   )
+});
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 25
+  },
+  filter: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly"
+  },
+  filterLabel: {
+    fontSize: 18,
+    marginRight: 10,
+    width: "80%"
+  }
 });
 
 export default Filters;
--- src/screens/Recipe.tsx
+++ src/screens/Recipe.tsx
@@ -1,18 +1,51 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, ScrollView, Image, StyleSheet, View } from "react-native";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import CustomHeaderButton from "../components/CustomHeaderButton";
 
 import { Recipe } from "../models/recipe";
+import { FlatList } from "react-native-gesture-handler";
 
 const RecipeScreen: NavigationStackScreenComponent = props => {
   const { navigation } = props;
 
   const recipe: Recipe = navigation.getParam("recipe");
 
-  return <Text>{recipe.title}</Text>;
+  return (
+    <ScrollView>
+      <Image
+        source={{ uri: recipe.imageUrl }}
+        style={styles.image}
+        resizeMode="cover"
+      />
+
+      <View>
+        <Text>Duration: {recipe.duration}</Text>
+        <Text>Level: {recipe.complexity}</Text>
+        <Text>Cost: {recipe.affordability}</Text>
+      </View>
+
+      <View>
+        <Text>Ingredients:</Text>
+        <FlatList
+          data={recipe.ingredients}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+
+      <View>
+        <Text>Steps:</Text>
+        <FlatList
+          data={recipe.steps}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    </ScrollView>
+  );
 };
 
 RecipeScreen.navigationOptions = navigationProps => {
@@ -27,5 +60,12 @@ RecipeScreen.navigationOptions = navigationProps => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300
+  }
+});
 
 export default RecipeScreen;
